87ecf684c0b5032451c1ece1f41b4485ab795869
Ania <ania@MBP-Ania.hq.kempniu.pl>
2022-04-03 18:39:00 +0200

    Dodanie wymaganych parametrów

--- IndegoTask.py
+++ IndegoTask.py
@@ -6,5 +6,5 @@ str1 = input()
 str2 = input()
 
 matcher = SequenceMatcher(None, str1, str2)
-result = matcher.find_longest_match()
+result = matcher.find_longest_match(0, len(str1), 0, len(str2))
 print(str1[result.a : result.a + result.size])
